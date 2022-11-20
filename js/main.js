6e43a2ce72f22a9043108077218741e925e98d92
Franz-SuperCode <franzsupercode@gmail.com>
2022-11-20 14:46:15 +0100

    fertig

---
+++ js/main.js
@@ -0,0 +1,64 @@
+let catEl = document.querySelector("#cat");
+let distanceWalk = 3;
+let timer;
+let speed = 50;
+
+//Beim klick wird die Katze losgehen
+function catWalk() {
+    timer = setInterval(addDistance, speed);
+}
+
+//Beendet den Intervall
+function pause() {
+    clearInterval(timer);
+    console.log("pause");
+}
+
+//Nach jedem turn wird die Katze wieder losgehen. Damit das "losgehen" nicht zweimal ausgeführt wird, wird es zu Beginn entfernt
+function turn() {
+    catEl.classList.toggle("turn");
+    clearInterval(timer);
+    timer = setInterval(addDistance, speed);
+    console.log("turn");
+
+}
+
+
+
+function addDistance() {
+    //Wenn die Katze an den linken Bildschirmrand kommt, soll der Intervall aufhören
+    if (window.innerWidth - 310 <= distanceWalk) {
+        clearInterval(timer);
+        distanceWalk -= 3;
+    }
+    //Wenn die Katze an den rechten Bildschirmrand kommt, soll der Intervall aufhören
+    if (distanceWalk === 0) {
+        clearInterval(timer);
+        distanceWalk = 3;
+    }
+
+    //Falls die Katze die Klasse "turn" hat, soll der Wert von distanceWalk abnehmen
+    if (catEl.classList.contains("turn")) {
+        catEl.style.right = (distanceWalk) + "px";
+        distanceWalk--;
+        console.log(distanceWalk);
+        //Sonst soll der Wert von distanceWalk zunehmen
+    } else {
+        catEl.style.right = (distanceWalk) + "px";
+        distanceWalk++;
+        console.log(distanceWalk);
+    }
+
+}
+
+function catSpeed() {
+    //Wenn speed in den Minnusbereich geht, dann auf 1 setzen und Funktion beenden
+    if (speed <= 0) {
+        speed = 1;
+        return
+    }
+    speed -= 10;
+    //Den Timer neustarten damit er den neuen Speed Wert verwendet
+    clearInterval(timer);
+    timer = setInterval(addDistance, speed);
+}
